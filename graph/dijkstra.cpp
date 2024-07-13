a6488b986910b532cdc72314676ca0ee89d2fa54
osmankhawaja <153643745+osmankhawaja@users.noreply.github.com>
2024-07-13 23:54:05 +0600

    Update dijkstra.cpp

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -1,29 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-#define int         long long
-#define test        int t; cin>>t; while(t--)
-#define ll          long long
-#define pb          push_back
-#define pob         pop_back
-#define forc(i, n)  for(int i=0; i<n; i++)
-#define yes         cout<<"YES\n"
-#define no          cout<<"NO\n"
-#define all(n)      n.begin(),n.end()
-#define rll(n)      n.rbegin(),n.rend()
-#define sz          size()
-#define vi          vector<int>
-#define vl          vector<ll>
-#define vc          vector<char>
-#define vs          vector<string>
-#define mp          map<int, int>
-#define ff          first
-#define ss          second
-#define endl        "\n"
-#define mem(a)      memset(a, -1, sizeof(a))
-#define setbit(a)   __builtin_popcount(a)
-
 int const INF=1e16;
+
 void dijkstra(int n, vector<vector<pair<int, int>>> &graph)
 {
     vector<int>dis(n+5, INF);
@@ -54,15 +32,14 @@ void dijkstra(int n, vector<vector<pair<int, int>>> &graph)
     {
         cout<<dis[i]<<" ";
     }
-
 }
 
-int32_t main()
+int main()
 {
     int n,m;
     cin>>n>>m;
     vector<vector<pair<int, int>>> graph(n+5);
-    forc(i, m)
+    for(int i=0; i<m; i++)
     {
         int x,y,wt;
         cin>>x>>y>>wt;
